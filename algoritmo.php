6cac61ff18969ffec9abf8a8b9156ef56c079b99
Marko <70534584+marcokodde@users.noreply.github.com>
2023-06-30 08:38:21 -0600

    improvement in view index

--- algoritmo.php
+++ algoritmo.php
@@ -1,21 +1,25 @@
 <?php
-$page_title = 'Datos de Array';
-/* Se valida que se obtenga un id valido */
-$alice = [99, 28, 30];
-$bob = [17, 16, 8];
-$result = [];
-$i = 0;
-$a = 0;
-$b = 0;
+if (isset($_POST['uno']) && $_POST['uno_b']) {
+    $alice[0] = $_POST['uno'];
+    $alice[1] = $_POST['dos'];
+    $alice[2] = $_POST['tres'];
 
-foreach ($alice as $clave => $valor) {
-    if ($alice[$clave] > $bob[$clave]) {
-        $a = $a + 1;
-    } else {
-        $b = $b + 1;
+    $bob[0] = $_POST['uno_b'];
+    $bob[1] = $_POST['dos_b'];
+    $bob[2] = $_POST['tres_b'];
+    $result = [];
+    $i = 0;
+    $a = 0;
+    $b = 0;
+
+    foreach ($alice as $clave => $valor) {
+        if ($alice[$clave] > $bob[$clave]) {
+            $a = $a + 1;
+        } else {
+            $b = $b + 1;
+        }
     }
+    $result[0] = $a;
+    $result[1] = $b;
+    print_r($result);
 }
-
-$result[0] = $a;
-$result[1] = $b;
-print_r($result);
--- index.php
+++ index.php
@@ -8,13 +8,38 @@
                 <div class="col-md-6">
                     <div class="card">
                         <form action="algoritmo.php" method="POST">
+                            <label for="alice">Ejemplo</label>
                             <fieldset>
                                 $alice = [17, 28, 30];
                             </fieldset>
                             <fieldset>
                                 $bob = [99, 16, 8];
                             </fieldset>
-                            <input class="form-control" type="text" name="enviar" value="Enviar">
+                            <label for="alice">Alice</label>
+                            <div class="row m-1">
+                                <div class="col-md-2">
+                                    <input class="form-control" type="number" min="1" max="99" name="uno" required>
+                                </div>
+                                <div class="col-md-2">
+                                    <input class="form-control" type="number" min="1" max="99" name="dos" required>
+                                </div>
+                                <div class="col-md-2">
+                                    <input class="form-control" type="number" min="1" max="99" name="tres" required>
+                                </div>
+                            </div>
+                            <label for="alice">Bob</label>
+                            <div class="row m-1">
+                                <div class="col-md-2">
+                                    <input class="form-control" type="number" min="1" max="99" name="uno_b" required>
+                                </div>
+                                <div class="col-md-2">
+                                    <input class="form-control" type="number" min="1" max="99" name="dos_b" required>
+                                </div>
+                                <div class="col-md-2">
+                                    <input class="form-control" type="number" min="1" max="99" name="tres_b" required>
+                                </div>
+                            </div>
+
                             <button class="btn btn-success" name="submit">
                                 Leer Algoritmo
                             </button>
